19ee7858b4d303e6fd6bcef420190e11fbb5278c
karloslazaroo <101194055+karloslazaroo@users.noreply.github.com>
2023-03-21 02:03:35 +0800

    navbar changes

---
+++ blog-website/src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <div className="appear">
+        
+           <a href='#' className='title'>Karlos Lazaro.</a>  
+        
+        <div className="pages">
+            <a>Home</a>
+            <a>Blogs</a>
+            <a>About Me</a>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
