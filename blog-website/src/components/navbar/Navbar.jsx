d8130246b96a553450dde888700e9853025f8ae9
karloslazaroo <101194055+karloslazaroo@users.noreply.github.com>
2023-03-21 16:27:22 +0800

    chnages

--- blog-website/src/components/navbar/Navbar.jsx
+++ blog-website/src/components/navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faInstagram, faLinkedinIn, faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 const Navbar = () => {
   return (
+  <div className="header">
     <div className='Navbar'>
         <div className="appear">
         
@@ -10,11 +13,13 @@ const Navbar = () => {
         
         <div className="pages">
             <a href="#" >Home</a>
-
-            <a href="#" >Blogs</a>
+            <a href="#" >Projects</a>
+            <a href="#" >Posts</a>
             <a href="#" >About Me</a>
         </div>
+        Log In
         </div>
+    </div>
     </div>
   )
 }
--- blog-website/src/pages/Home/Homepage.jsx
+++ blog-website/src/pages/Home/Homepage.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import './homepage.css'
 import Navbar from '../../components/navbar/Navbar'
-
+import image from '../../assets/image.jpeg'
 
 const Homepage = () => {
   return (
     
-    <div>
+    <div className='body'>
         < Navbar/>
+
+        <div className="picture">
+          <div className="image">
+            <img src={image}></img>
+          </div>
+        </div>
 
         
     </div>
